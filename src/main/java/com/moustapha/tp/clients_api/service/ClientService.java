9950e5fb669103899971698e8e93abd544eb59ef
Moustapha <moustapha.bouzou@dev4smart.net>
2026-08-10 11:39:23 +0100

    Fix cache eviction on create/delete

--- src/main/java/com/moustapha/tp/clients_api/service/ClientService.java
+++ src/main/java/com/moustapha/tp/clients_api/service/ClientService.java
@@ -1,10 +1,9 @@
 package com.moustapha.tp.clients_api.service;
-
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
 import com.moustapha.tp.clients_api.model.Client;
 import com.moustapha.tp.clients_api.repository.ClientRepository;
 
@@ -22,6 +21,7 @@ public class ClientService {
         return clientRepository.findAll();
     }
 
+    @CacheEvict(value = "clients", allEntries = true)
     public Client createOrUpdate(Client client) {
         return clientRepository.save(client);
     }
@@ -30,6 +30,7 @@ public class ClientService {
         return clientRepository.findById(id).orElse(null);
     }
 
+    @CacheEvict(value = "clients", allEntries = true)
     public boolean deleteClient(Long id) {
         clientRepository.deleteById(id);
         return true;
